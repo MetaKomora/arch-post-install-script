643f1d5ee80de3446f055c2a179995e459985f2f
MetaKomora <mateusmartinsmk@outlook.com>
2024-10-08 15:42:59 -0300

    Change user environment setup
    - Separate installation for some Flatpaks to have a faster and more minimalistic worflow.
    
    - Add more verbosity to remove/move/rename actions.
    
    - Use mise executable directly to fix problems.
    
    - Set some default programs with xdg-mime for both KDE and Hyprland environments.
    
    - Remove adw-gtk3 theme because Gradience was discontinued.
    
    - Add starship initialization to zshrc file.
    
    - Add SDDM theme installation and configuration for both environments.

--- arch-post-install-script.sh
+++ arch-post-install-script.sh
@@ -20,11 +20,11 @@ function initialSystemSetup() {
 	# Disable pcspeaker sound on boot
 	echo "blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf
 
-	sudo mkdir -p /etc/zsh
+	sudo mkdir -pv /etc/zsh
 	echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee -a /etc/zsh/zshenv
 
 	# Making some directories and exporting variables to easy setup later
-	mkdir -p $HOME/.config/{zsh,zim} $HOME/.local/{bin,share} $HOME/.local/share/icons $HOME/{.icons,.themes} $HOME/.var/app
+	mkdir -pv $HOME/.config/{zsh,zim} $HOME/.local/{bin,share} $HOME/.local/share/icons $HOME/{.icons,.themes} $HOME/.var/app
 	
 	printf '%s\n' \
         'export XDG_CONFIG_HOME=$HOME/.config' \
@@ -82,14 +82,16 @@ function installPrograms() {
 	    cd yay-bin
 	    makepkg -si --noconfirm --needed
 	    cd $HOME
-	    rm -rf yay-bin
+	    rm -rfv yay-bin
 	}
 	fi
 
 	# Grants Flatpak read access to all possible locations for themes and icons inside $HOME directory and Mangohud config read access
 	sudo flatpak override --filesystem=~/.themes:ro --filesystem=~/.icons:ro --filesystem=~/.local/share/icons:ro --filesystem=~/.local/share/themes:ro --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtk-4.0:ro --filesystem=xdg-config/MangoHud:ro --env=XCURSOR_PATH=~/.icons
 	
-	flatpak install org.mozilla.firefox org.mozilla.Thunderbird org.chromium.Chromium org.telegram.desktop com.discordapp.Discord im.riot.Riot org.libreoffice.LibreOffice org.gnome.papers org.gnome.clocks org.gnome.Calculator io.freetubeapp.FreeTube io.mpv.Mpv io.missioncenter.MissionCenter com.github.johnfactotum.Foliate com.github.finefindus.eyedropper com.usebruno.Bruno com.obsproject.Studio com.valvesoftware.Steam net.minetest.Minetest com.heroicgameslauncher.hgl org.libretro.RetroArch org.freedesktop.Platform.VulkanLayer.MangoHud org.freedesktop.Platform.VulkanLayer.gamescope page.kramo.Cartridges -y
+	flatpak install org.mozilla.firefox org.mozilla.Thunderbird org.chromium.Chromium org.telegram.desktop com.valvesoftware.Steam io.freetubeapp.FreeTube org.gnome.papers -y
+
+	flatpak install com.discordapp.Discord im.riot.Riot org.libreoffice.LibreOffice org.gnome.clocks org.gnome.Calculator io.mpv.Mpv io.missioncenter.MissionCenter com.github.johnfactotum.Foliate com.github.finefindus.eyedropper com.usebruno.Bruno com.obsproject.Studio net.minetest.Minetest com.heroicgameslauncher.hgl org.libretro.RetroArch org.freedesktop.Platform.VulkanLayer.MangoHud org.freedesktop.Platform.VulkanLayer.gamescope page.kramo.Cartridges -y
 	
 	# Grants Freetube access to session bus to be able to open videos on MPV
 	sudo flatpak override --socket=session-bus io.freetubeapp.FreeTube
@@ -135,11 +137,10 @@ function devEnvironmentSetup() {
     # Mise installation and activation to use now
 	curl https://mise.run | sh
     echo "eval \"\$($HOME/.local/bin/mise activate zsh)\"" >> "$HOME/.config/zsh/.zshrc"
-    mise activate bash
 
     # Mise and pnpm completions and Mise plugins installation
-    mise completion zsh >> "$HOME/.config/zsh/.zshrc"
-    mise use -g -y usage node@20 pnpm shellcheck
+    $HOME/.local/bin/mise completion zsh >> "$HOME/.config/zsh/.zshrc"
+    $HOME/.local/bin/mise use -g -y usage node@20 pnpm shellcheck
     pnpm setup
 
 }
@@ -149,25 +150,24 @@ function userEnvironmentSetup() {
 
 	# Setting XDG directories and some default applications
 	xdg-user-dirs-update
-	xdg-mime default org.gnome.Loupe.desktop image/png
-	xdg-mime default org.gnome.Loupe.desktop image/jpeg
-	xdg-mime default org.gnome.Loupe.desktop image/webp
-	xdg-mime default org.gnome.Evince.desktop application/pdf
+	xdg-mime default org.gnome.Papers.desktop application/pdf
 	xdg-mime default org.mozilla.firefox.desktop x-scheme-handler/http
 	xdg-mime default org.mozilla.firefox.desktop x-scheme-handler/https
 
+    # Downloading and extracting icon and cursor themes
+	cd $HOME
+	git clone https://github.com/vinceliuice/Tela-circle-icon-theme
+	curl -L -O "https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Ice.tar.xz"
+	tar -xvf Bibata-Modern-Ice.tar.xz
+
     if [[ $GTKENV == true ]]; then {
-        # Install GTK, icon and cursor themes
-        cd $HOME
-        curl -L -O "$(curl "https://api.github.com/repos/lassekongo83/adw-gtk3/releases" | jq -r '.[0].assets[0].browser_download_url')"
-        tar -xvf adw-gtk3v*.tar.xz;
-        rm -rf adw-gtk3v*.tar.xz;
-        mv adw-gtk3 adw-gtk3-dark $HOME/.themes
-        git clone https://github.com/vinceliuice/Tela-circle-icon-theme
-        cd Tela-circle-icon-theme; ./install.sh -d $HOME/.icons; cd ../
-        curl -L -O "https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Ice.tar.xz"
-        tar -xvf Bibata-Modern-Ice.tar.xz
-        mv Bibata-Modern-Ice $HOME/.icons
+
+        xdg-mime default org.gnome.Loupe.desktop image/png
+        xdg-mime default org.gnome.Loupe.desktop image/jpeg
+        xdg-mime default org.gnome.Loupe.desktop image/webp
+
+        cd $HOME/Tela-circle-icon-theme; ./install.sh -d $HOME/.icons
+        mv -v $HOME/Bibata-Modern-Ice $HOME/.icons
         cd $HOME
 
         # Themes configuration
@@ -189,19 +189,17 @@ function userEnvironmentSetup() {
         gsettings set org.gtk.gtk4.Settings.FileChooser show-hidden true
 
     } else {
-        cd $HOME
-        sudo pacman -Syu git jq wget unzip qt6-svg qt6-declarative --noconfirm --needed
-        git clone https://github.com/vinceliuice/Tela-circle-icon-theme; cd Tela-circle-icon-theme/; ./install.sh
 
-        curl -L -O "https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Ice.tar.xz"; 
-        tar -xvf Bibata-Modern-Ice.tar.xz; mv Bibata-Modern-Ice $HOME/.local/share/icons/
+        xdg-mime default org.kde.gwenview.desktop image/png
+        xdg-mime default org.kde.gwenview.desktop image/jpeg
+        xdg-mime default org.kde.gwenview.desktop image/webp
+
+        cd $HOME/Tela-circle-icon-theme; ./install.sh -d $HOME/.local/share/icons
+        mv -v $HOME/Bibata-Modern-Ice $HOME/.local/share/icons
+        cd $HOME
 
         git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde; ./install.sh;
-
-        curl -L -O "$(curl "https://api.github.com/repos/catppuccin/sddm/releases" | jq -r '.[0].assets[3].browser_download_url')"; sudo mv catppuccin-mocha /usr/share/sddm/themes/
         ln -s ~/.local/share/icons/ ~/.icons
-        sudo ln -sf ~/.local/share/icons/Bibata-Modern-Ice /usr/share/icons/Bibata-Modern-Ice; sudo ln -sf ~/.local/share/icons/Tela-circle /usr/share/icons/Tela-circle; sudo ln -sf ~/.local/share/icons/Tela-circle-light /usr/share/icons/Tela-circle-light; sudo ln -sf ~/.local/share/icons/Tela-circle-dark /usr/share/icons/Tela-circle-dark
-        cd $HOME
 
     }
     fi
@@ -210,9 +208,16 @@ function userEnvironmentSetup() {
     ya pack -a "yazi-rs/flavors#catppuccin-mocha"
 
 	# Cleanup
-	rm -rf Tela-circle-icon-theme Bibata-Modern-Ice.tar.xz .npm
-	rm .bashrc .bash_profile .bash_logout .bash_history
-	sudo pacman -Rn gnu-free-fonts --noconfirm
+	rm -rfv Tela-circle-icon-theme Bibata-Modern-Ice.tar.xz .npm
+	rm -v .bashrc .bash_profile .bash_logout .bash_history
+
+	echo "eval \"\$(starship init zsh)\"" >> "$HOME/.config/zsh/.zshrc"
+	
+}
+
+function systemTweaks() {
+
+	printMessage "$1"
 
 	# Activate conservation mode on Ideapad laptops
 	echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
@@ -220,18 +225,8 @@ function userEnvironmentSetup() {
 	# Enable power-profiles-daemon
 	sudo systemctl enable power-profiles-daemon
 
-	# Change shell to ZSH
-	chsh -s /usr/bin/zsh
-	sudo chsh -s /usr/bin/zsh
-	source $HOME/.config/zsh/.zshenv
-
-	# Downloading Zim Framework
-	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-}
-
-function enableZRAM() {
-
-	printMessage "$1"
+    # Remove gnu-free-fonts since noto-fonts is already installed
+	sudo pacman -Rn gnu-free-fonts --noconfirm
 	
 	# Enable zram module
 	sudo sed -i "s/MODULES=()/MODULES=(zram)/" /etc/mkinitcpio.conf
@@ -245,6 +240,29 @@ function enableZRAM() {
 	
 	# Optimizing swap on zram
 	printf "vm.swappiness = 180\nvm.watermark_boost_factor = 0\nvm.watermark_scale_factor = 125\nvm.page-cluster = 0" | sudo tee -a /etc/sysctl.d/99-vm-zram-parameters.conf
+
+    # Installing and configuring SDDM theme
+	sudo pacman -S qt6-svg qt6-declarative --noconfirm --needed
+    sudo mkdir -pv /etc/sddm.conf.d/ /usr/share/sddm/themes/
+    curl -L -O "$(curl "https://api.github.com/repos/catppuccin/sddm/releases" | jq -r '.[0].assets[3].browser_download_url')"; 
+    sudo mv -v catppuccin-mocha /usr/share/sddm/themes/
+
+    printf '%s\n' \
+        '[Theme]' \
+        'Current=catppuccin-mocha' \
+        '[General]' \
+        'GreeterEnvironment=QT_SCREEN_SCALE_FACTORS=1.25' \
+        '[Wayland]' \
+        'EnableHiDPI=true' >> /etc/sddm.conf.d/default.conf
+
+    # Change shell to ZSH
+	chsh -s /usr/bin/zsh
+	sudo chsh -s /usr/bin/zsh
+	source $HOME/.config/zsh/.zshenv
+
+	# Downloading Zim Framework
+	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
 }
 
 
@@ -264,7 +282,7 @@ devEnvironmentSetup "Installing development tools"
 
 userEnvironmentSetup "Setting default applications, installing themes and making cleanups"
 
-enableZRAM "Enabling and configuring ZRAM"
+systemTweaks "Enabling, configuring ZRAM and other tweaks"
 
 printMessage "Please, reboot system to apply changes"
 
