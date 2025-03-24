0e6bae962e92636a451ece5c09c43f3686db295d
MetaKomora <mateusmartinsmk@outlook.com>
2025-03-23 21:55:40 -0300

    - Swith Bottles and Cartridges to Lutris - Remove Discord - Use --env=STEAM_FORCE_DESKTOPUI_SCALING=1.25 on both environments - Disabled pnpm setup until proper fix - If zram-generator is not installed, setup zram udev rule - Remove Canada from Pacman Mirrorlist - Switch .zshenv to .zshrc

--- arch-post-install-script.sh
+++ arch-post-install-script.sh
@@ -12,7 +12,7 @@ function initialSystemSetup() {
 
     # Change mirrorlist
     sudo pacman -Syyu reflector --noconfirm --needed
-    sudo reflector --country Sweden,United_States,Canada --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+    sudo reflector --country Sweden,United_States --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
     # Uncommenting some options on Pacman config
     sudo sed -i -e 's/#Color/Color/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
@@ -32,7 +32,7 @@ function initialSystemSetup() {
         'export XDG_CACHE_HOME=$HOME/.cache' \
         'export XDG_DATA_HOME=$HOME/.local/share' \
         'export HISTFILE=$HOME/.config/zsh/zhistory' \
-        'export ZIM_HOME=$HOME/.config/zim' >> $HOME/.config/zsh/.zshenv
+        'export ZIM_HOME=$HOME/.config/zim' >> $HOME/.config/zsh/.zshrc
 
 }
 
@@ -89,13 +89,13 @@ function installPrograms() {
 	
     flatpak install org.mozilla.firefox app.zen_browser.zen org.mozilla.Thunderbird org.chromium.Chromium org.telegram.desktop com.valvesoftware.Steam io.freetubeapp.FreeTube org.gnome.Papers -y
 
-    flatpak install com.discordapp.Discord im.riot.Riot org.libreoffice.LibreOffice org.gnome.clocks org.gnome.Calculator io.mpv.Mpv io.missioncenter.MissionCenter com.github.johnfactotum.Foliate io.github.josephmawa.Bella com.usebruno.Bruno com.obsproject.Studio com.heroicgameslauncher.hgl org.libretro.RetroArch org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08 org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08 com.valvesoftware.Steam.CompatibilityTool.Proton-GE page.kramo.Cartridges com.usebottles.bottles -y
+    flatpak install im.riot.Riot org.libreoffice.LibreOffice org.gnome.clocks org.gnome.Calculator io.mpv.Mpv io.missioncenter.MissionCenter com.github.johnfactotum.Foliate io.github.josephmawa.Bella com.usebruno.Bruno com.obsproject.Studio net.lutris.Lutris com.heroicgameslauncher.hgl org.libretro.RetroArch org.freedesktop.Platform.VulkanLayer.MangoHud//23.08 org.freedesktop.Platform.VulkanLayer.gamescope//23.08 com.valvesoftware.Steam.CompatibilityTool.Proton-GE -y
 	
     # Grants Freetube access to session bus to be able to open videos on MPV
     sudo flatpak override --socket=session-bus io.freetubeapp.FreeTube
 
-    # Enable Mangohud on Steam Games and grants Steam access to Games directory
-    sudo flatpak override --env=MANGOHUD=1 --filesystem=$HOME/Games com.valvesoftware.Steam
+    # Enable Mangohud on Steam games, grants Steam access to Games directory and force Steam fractional scaling
+    sudo flatpak override --env=STEAM_FORCE_DESKTOPUI_SCALING=1.25 --env=MANGOHUD=1 --filesystem=$HOME/Games com.valvesoftware.Steam
 
     # Enable Wayland support on Thunderbird
     sudo flatpak override --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.Thunderbird
@@ -105,20 +105,18 @@ function installPrograms() {
     printf "--ozone-platform-hint=auto" > $HOME/.var/app/org.chromium.Chromium/config/chromium-flags.conf
 	
     if [[ $GTKENV == true ]]; then {
+        # Install these packages just for Hyprland
         flatpak install com.github.tchx84.Flatseal org.flameshot.Flameshot org.gnome.Calendar org.gnome.Loupe com.saivert.pwvucontrol -y
 
         # To Flameshot properly work on Hyprland
         sudo flatpak override --env=XDG_CURRENT_DESKTOP=sway org.flameshot.Flameshot
 
-        # For Telegram Pop-ups to work properly
+        # For Telegram Pop-ups to work properly on Hyprland
         sudo flatpak override --env=QT_WAYLAND_DISABLED_INTERFACES=wp_fractional_scale_manager_v1 org.telegram.desktop
 
     } else {
+        # Install gwenview just for KDE
         flatpak install org.kde.gwenview -y
-
-        # Force Steam scaling manually
-        sudo flatpak override --env=STEAM_FORCE_DESKTOPUI_SCALING=1.25 com.valvesoftware.Steam
-
     }
     fi
 	
@@ -136,7 +134,7 @@ function devEnvironmentSetup() {
     # Mise and pnpm completions and Mise plugins installation
     $HOME/.local/bin/mise completion zsh >> "$HOME/.config/zsh/.zshrc"
     $HOME/.local/bin/mise use -g -y usage node@20 pnpm shellcheck@0.9.0
-    pnpm setup
+    #pnpm setup # Manually use the command later, until proper fix
 
 }
 
@@ -202,19 +200,23 @@ function systemTweaks() {
     # Remove gnu-free-fonts since noto-fonts is already installed
     sudo pacman -Rn gnu-free-fonts --noconfirm
 	
-    # Enable zram module
-    sudo sed -i "s/MODULES=()/MODULES=(zram)/" /etc/mkinitcpio.conf
-    echo "zram" | sudo tee -a /etc/modules-load.d/zram.conf
+    # if zram-generator is not installed, setup zram udev rule
+    if [[ $(pacman -Qq zram-generator 2>/dev/null) != "zram-generator" ]]; then {
+        # Enable zram module
+        sudo sed -i "s/MODULES=()/MODULES=(zram)/" /etc/mkinitcpio.conf
+        echo "zram" | sudo tee /etc/modules-load.d/zram.conf
 	
-    # Create a udev rule. Change ATTR{disksize} to your needs
-    echo 'ACTION=="add", KERNEL=="zram0", ATTR{initstate}=="0", ATTR{comp_algorithm}="zstd", ATTR{disksize}="4G", RUN="/usr/bin/mkswap -U clear %N", TAG+="systemd"' | sudo tee -a /etc/udev/rules.d/99-zram.rules
+        # Create a udev rule. Change ATTR{disksize} to your needs
+        echo 'ACTION=="add", KERNEL=="zram0", ATTR{initstate}=="0", ATTR{comp_algorithm}="zstd", ATTR{disksize}="4G", RUN="/usr/bin/mkswap -U clear %N", TAG+="systemd"' | sudo tee /etc/udev/rules.d/99-zram.rules
 	
-    # Add /dev/zram to your fstab
-    echo "/dev/zram0 none swap defaults,discard,pri=100 0 0" | sudo tee -a /etc/fstab
+        # Add /dev/zram to your fstab
+        echo "/dev/zram0 none swap defaults,discard,pri=100 0 0" | sudo tee -a /etc/fstab
 	
-    # Optimizing swap on zram
-    printf "vm.swappiness = 180\nvm.watermark_boost_factor = 0\nvm.watermark_scale_factor = 125\nvm.page-cluster = 0" | sudo tee -a /etc/sysctl.d/99-vm-zram-parameters.conf
-
+        # Optimizing swap on zram
+        printf "vm.swappiness = 180\nvm.watermark_boost_factor = 0\nvm.watermark_scale_factor = 125\nvm.page-cluster = 0" | sudo tee /etc/sysctl.d/99-vm-zram-parameters.conf
+    }
+    fi
+    
     # Installing and configuring SDDM theme
     sudo systemctl enable sddm
     sudo pacman -S qt6-svg qt6-declarative --noconfirm --needed
@@ -252,7 +254,7 @@ function systemTweaks() {
     # Change shell to ZSH
     chsh -s /usr/bin/zsh
     sudo chsh -s /usr/bin/zsh
-    source $HOME/.config/zsh/.zshenv
+    source $HOME/.config/zsh/.zshrc
 
     # Downloading Zim Framework
     curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
